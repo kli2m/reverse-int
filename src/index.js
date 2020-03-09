8a7a406f119e034c69c771062a574d31cf637596
kli2m <ivanklimm87@gmail.com>
2020-03-09 22:18:19 +0300

    feat: task finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = n.toString()
+    if (n < 0) return str.substr(1).split("").reverse().join("")
+    return str.split("").reverse().join("")
 }
